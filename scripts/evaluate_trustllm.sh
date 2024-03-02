cafebcba516d664f146c082955bda815f12ac9bc
Allyson Ettinger <allysone@allenai.org>
2024-03-01 21:02:59 -0500

    privacy only

--- scripts/evaluate_trustllm.sh
+++ scripts/evaluate_trustllm.sh
@@ -2,9 +2,9 @@
 
 python evaluation/trustllm/pipeline.py \
     --filename tulu-2-7b.json \
-    --safety_dir result_dirs/trustllm/safety/ \
+    # --safety_dir result_dirs/trustllm/safety/ \
     --privacy_dir result_dirs/trustllm/privacy/ \
-    --fairness_dir result_dirs/trustllm/fairness/ \
-    --truthfulness_dir result_dirs/trustllm/truthfulness/ \
-    --robustness_dir result_dirs/trustllm/robustness/ \
-    --ethics_dir result_dirs/trustllm/ethics/ \
+    # --fairness_dir result_dirs/trustllm/fairness/ \
+    # --truthfulness_dir result_dirs/trustllm/truthfulness/ \
+    # --robustness_dir result_dirs/trustllm/robustness/ \
+    # --ethics_dir result_dirs/trustllm/ethics/ \
