dc8781d26ea134120f8249d18c0e18484debb62a
Allyson Ettinger <allysone@allenai.org>
2024-03-01 21:04:12 -0500

    privacy only

--- scripts/evaluate_trustllm.sh
+++ scripts/evaluate_trustllm.sh
@@ -2,8 +2,8 @@
 
 python evaluation/trustllm/pipeline.py \
     --filename tulu-2-7b.json \
-    # --safety_dir result_dirs/trustllm/safety/ \
     --privacy_dir result_dirs/trustllm/privacy/ \
+    # --safety_dir result_dirs/trustllm/safety/ \
     # --fairness_dir result_dirs/trustllm/fairness/ \
     # --truthfulness_dir result_dirs/trustllm/truthfulness/ \
     # --robustness_dir result_dirs/trustllm/robustness/ \
