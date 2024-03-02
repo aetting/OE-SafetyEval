88df7b2116e61caf9dab1dcd9c49059f2d13c890
Allyson Ettinger <allysone@allenai.org>
2024-03-01 23:36:25 -0500

    fixed  advfact

--- scripts/evaluate_trustllm.sh
+++ scripts/evaluate_trustllm.sh
@@ -1,17 +1,18 @@
 
 
-# python evaluation/trustllm/pipeline.py \
-#     --filename tulu-2-7b.json \
-#     --fairness_dir result_dirs/trustllm/fairness/ \
+python evaluation/trustllm/pipeline.py \
+    --filename tulu-2-7b.json \
+    --truthfulness_dir result_dirs/trustllm/truthfulness/ \
+    # --fairness_dir result_dirs/trustllm/fairness/ \
 #     # --privacy_dir result_dirs/trustllm/privacy/ \
 #     # --safety_dir result_dirs/trustllm/safety/ \
 #     # --truthfulness_dir result_dirs/trustllm/truthfulness/ \
 #     # --robustness_dir result_dirs/trustllm/robustness/ \
 #     # --ethics_dir result_dirs/trustllm/ethics/ \
 
-python evaluation/trustllm/all_eval.py \
-    --data_dir /net/nfs.cirrascale/mosaic/allysone/safety-eval/other_generations/allenai-tulu-2-7b-final_merged_checkpoint \
-    --truthfulness \
-    # --safety \
-    # --privacy \
-    # --fairness 
+# python evaluation/trustllm/all_eval.py \
+#     --data_dir /net/nfs.cirrascale/mosaic/allysone/safety-eval/other_generations/allenai-tulu-2-7b-final_merged_checkpoint \
+#     --truthfulness \
+#     # --safety \
+#     # --privacy \
+#     # --fairness 
