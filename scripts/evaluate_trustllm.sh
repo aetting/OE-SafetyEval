641fa1a30ecdd98cc008abd3d4ffa5b014ac78b1
Allyson Ettinger <allysone@allenai.org>
2024-03-04 09:25:13 -0800

    firt 5 epoch

--- scripts/evaluate_trustllm.sh
+++ scripts/evaluate_trustllm.sh
@@ -11,7 +11,7 @@
 #     # --ethics_dir result_dirs/trustllm/ethics/ \
 
 python evaluation/trustllm/all_eval.py \
-    --data_dir /net/nfs.cirrascale/mosaic/allysone/safety-eval/other_generations/tulu-7b-uncensored--final_merged_checkpoint \
+    --data_dir /net/nfs.cirrascale/mosaic/allysone/safety-eval/other_generations/epoch5/tulu-7b-uncensored--final_merged_checkpoint \
     --safety \
     --privacy \
     --truthfulness \
