837b3a493dd8de1ab9cdc4ca1e6c47eac026a2f8
Allyson Ettinger <allysone@allenai.org>
2024-03-04 16:35:27 -0800

    rename

--- scripts/refusal_eval.sh
+++ scripts/refusal_eval.sh
@@ -1,5 +1,5 @@
 
-python evaluation/refusal/do_anything_now_4way.py \
+python evaluation/refusal/gpt_rubric_eval.py \
     --output_file  /net/nfs.cirrascale/mosaic/allysone/tulu-eval/dan-generations/results-7b-testing/with-jailbreaks-predictions.jsonl \
     --results_file /net/nfs.cirrascale/mosaic/allysone/tulu-eval/dan-generations/jb_testing.jsonl \
     --rubric_type do_anything_now \
