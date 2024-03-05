21ef1a8bfc5a4303e8c64903398c71d5291855b5
Allyson Ettinger <allysone@allenai.org>
2024-03-04 16:08:36 -0800

    consolidate refusal eval prompts

--- scripts/refusal_eval.sh
+++ scripts/refusal_eval.sh
@@ -1,5 +1,5 @@
 
-python evaluation/refusal/resp_eval_jb.py \
+python evaluation/refusal/do_anything_now_4way.py \
     --output_file  /net/nfs.cirrascale/mosaic/allysone/tulu-eval/dan-generations/results-7b-testing/with-jailbreaks-predictions.jsonl \
     --results_file /net/nfs.cirrascale/mosaic/allysone/tulu-eval/dan-generations/jb_testing.jsonl \
     --rubric_type do_anything_now \
