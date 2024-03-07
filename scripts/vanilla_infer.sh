da115a05fb936e0a1f0c9bfbb4049ffb1ec4ed59
Allyson Ettinger <allysone@allenai.org>
2024-03-07 10:22:17 -0500

    example scripts

---
+++ scripts/vanilla_infer.sh
@@ -0,0 +1,26 @@
+model_name="allenai/tulu-2-dpo-7b"
+TEMP=0.7; TOP_P=1.0; MAX_TOKENS=512;
+
+#model_name="/net/nfs.cirrascale/allennlp/hamishi/checkpoints/olmo_7b_finetune_dpo"
+#model_name="/net/nfs.cirrascale/mosaic/allysone/soups/llama2/llama2-7b-chat/llama2-7b-chat-model"
+
+#model_name="allenai/OLMo-7B"
+
+# model_name="openai/gpt-3.5-turbo-0125"
+# TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
+
+num_gpus=1
+python src/unified_infer.py \
+    --model_name $model_name \
+    --engine openai \
+    --output_folder ./result_dirs/safety/ \
+    --data_name safetyx \
+    --data_file ../../tulu-eval/TrustLLM/dataset/truthfulness/internal.json \
+    --tensor_parallel_size $num_gpus \
+    --dtype bfloat16 \
+    --top_p $TOP_P \
+    --end_index 6 \
+    --temperature $TEMP \
+    --max_tokens $MAX_TOKENS \
+    --overwrite \
+    --hold_run
