a7c48d683e0b861f4ba42edff64cbb7265419e60
Allyson Ettinger <allysone@allenai.org>
2024-03-07 12:59:21 -0500

    test refusal eval

--- scripts/refusal_eval.sh
+++ scripts/refusal_eval.sh
@@ -1,7 +1,7 @@
 
 python evaluation/refusal/gpt_rubric_eval.py \
-    --output_file  ./result_dirs/refusal/DAN/with-jailbreaks-predictions.jsonl \
-    --results_file ./result_dirs/refusal/DAN/refusal.jsonl \
+    --output_file  ./result_dirs/refusal/DAN/jb/tulu-2-7b.0-10.json \
+    --results_file ./result_dirs/refusal/DAN/jb/results_tulu-2-7b.0-10.json \
     --model gpt-4-0613 \
     --rubric_type do_anything_now \
     --num_items 5 \
