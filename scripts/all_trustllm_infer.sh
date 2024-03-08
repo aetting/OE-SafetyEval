faaef830aec5ad9252c5e5f19ac326b897605a5c
Allyson Ettinger <allysone@allenai.org>
2024-03-08 18:23:11 -0500

    run all

--- scripts/all_trustllm_infer.sh
+++ scripts/all_trustllm_infer.sh
@@ -43,68 +43,68 @@ ethics=(
 )
 
 
-end_index=4
+end_index=-1
 
-# num_gpus=1
-# # for AREA in safety fairness truthfulness privacy robustness ethics; do
-# TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
-# batch_size=4
-# for AREA in safety privacy; do
-#     echo $AREA
-#     areaArray=$AREA[@]
-#     for FILE in ${!areaArray}; do
-#         echo $FILE
-#         DATA_NAME=${FILE%.*}
-#         for MODEL in "allenai/tulu-2-7b" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-13b" "allenai/tulu-2-13b"; do 
-#         echo $MODEL
-#         # CUDA_VISIBLE_DEVICES=$gpu \
-#         python src/unified_infer.py \
-#             --engine vllm \
-#             --model_name $MODEL \
-#             --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
-#             --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
-#             --tensor_parallel_size $num_gpus \
-#             --dtype bfloat16 \
-#             --top_p $TOP_P \
-#             --temperature $TEMP \
-#             --max_tokens $MAX_TOKENS \
-#             --batch_size $batch_size \
-#             --end_index $end_index \
-#             --overwrite
-#         done
-#     done
-# done
+num_gpus=1
+# for AREA in safety fairness truthfulness privacy robustness ethics; do
+TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
+batch_size=4
+for AREA in safety fairness truthfulness privacy; do
+    echo $AREA
+    areaArray=$AREA[@]
+    for FILE in ${!areaArray}; do
+        echo $FILE
+        DATA_NAME=${FILE%.*}
+        for MODEL in "allenai/tulu-2-dpo-13b" "allenai/tulu-2-13b" "allenai/tulu-2-7b" "allenai/tulu-2-dpo-7b"; do 
+        echo $MODEL
+        # CUDA_VISIBLE_DEVICES=$gpu \
+        python src/unified_infer.py \
+            --engine vllm \
+            --model_name $MODEL \
+            --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
+            --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
+            --tensor_parallel_size $num_gpus \
+            --dtype bfloat16 \
+            --top_p $TOP_P \
+            --temperature $TEMP \
+            --max_tokens $MAX_TOKENS \
+            --batch_size $batch_size \
+            --end_index $end_index \
+            --overwrite
+        done
+    done
+done
 
 
-# num_gpus=4
-# # for AREA in safety fairness truthfulness privacy robustness ethics; do
-# TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
-# batch_size=4
-# for AREA in safety privacy; do
-#     echo $AREA
-#     areaArray=$AREA[@]
-#     for FILE in ${!areaArray}; do
-#         echo $FILE
-#         DATA_NAME=${FILE%.*}
-#         for MODEL in "allenai/tulu-2-70b" "allenai/tulu-2-dpo-70b"; do 
-#         echo $MODEL
-#         # CUDA_VISIBLE_DEVICES=$gpu \
-#         python src/unified_infer.py \
-#             --engine vllm \
-#             --model_name $MODEL \
-#             --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
-#             --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
-#             --tensor_parallel_size $num_gpus \
-#             --dtype bfloat16 \
-#             --top_p $TOP_P \
-#             --temperature $TEMP \
-#             --max_tokens $MAX_TOKENS \
-#             --batch_size $batch_size \
-#             --end_index $end_index \
-#             --overwrite
-#         done
-#     done
-# done
+num_gpus=4
+# for AREA in safety fairness truthfulness privacy robustness ethics; do
+TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
+batch_size=4
+for AREA in safety fairness truthfulness privacy; do
+    echo $AREA
+    areaArray=$AREA[@]
+    for FILE in ${!areaArray}; do
+        echo $FILE
+        DATA_NAME=${FILE%.*}
+        for MODEL in "allenai/tulu-2-70b" "allenai/tulu-2-dpo-70b"; do 
+        echo $MODEL
+        # CUDA_VISIBLE_DEVICES=$gpu \
+        python src/unified_infer.py \
+            --engine vllm \
+            --model_name $MODEL \
+            --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
+            --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
+            --tensor_parallel_size $num_gpus \
+            --dtype bfloat16 \
+            --top_p $TOP_P \
+            --temperature $TEMP \
+            --max_tokens $MAX_TOKENS \
+            --batch_size $batch_size \
+            --end_index $end_index \
+            --overwrite
+        done
+    done
+done
 
 TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
 python src/unified_infer.py \
