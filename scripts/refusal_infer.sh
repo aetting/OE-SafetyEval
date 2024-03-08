781204c70f93906da724525d959c81d4f7d673ca
Allyson Ettinger <allysone@allenai.org>
2024-03-08 12:39:20 -0500

    multi-gpu testing

--- scripts/refusal_infer.sh
+++ scripts/refusal_infer.sh
@@ -1,4 +1,4 @@
-model_name="allenai/tulu-2-dpo-7b"
+model_name="allenai/tulu-2-dpo-13b"
 TEMP=0; TOP_P=1.0; MAX_TOKENS=512;
 
 #model_name="/net/nfs.cirrascale/allennlp/hamishi/checkpoints/olmo_7b_finetune_dpo"
@@ -9,7 +9,7 @@ TEMP=0; TOP_P=1.0; MAX_TOKENS=512;
 # model_name="openai/gpt-3.5-turbo-0125"
 # TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
 
-num_gpus=1
+num_gpus=2
 python src/unified_infer.py \
     --model_name $model_name \
     --engine vllm \
@@ -19,7 +19,7 @@ python src/unified_infer.py \
     --tensor_parallel_size $num_gpus \
     --dtype bfloat16 \
     --top_p $TOP_P \
-    --end_index 10 \
+    --end_index 4 \
     --temperature $TEMP \
     --max_tokens $MAX_TOKENS \
     --overwrite
