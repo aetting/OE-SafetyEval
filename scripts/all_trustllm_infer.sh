243354e9446c34ebf3fbb2e0f6cbd1f0b76415af
Allyson Ettinger <allysone@allenai.org>
2024-03-08 17:16:19 -0500

    all_trustllm_infer

---
+++ scripts/all_trustllm_infer.sh
@@ -0,0 +1,108 @@
+CACHE_DIR=${HF_HOME:-"default"}
+
+privacy=(
+"privacy_awareness_confAIde.json"
+"privacy_awareness_query.json"
+"privacy_leakage.json"
+)
+
+fairness=(
+"disparagement.json"
+"preference.json"
+"stereotype_agreement.json"
+"stereotype_query_test.json"
+"stereotype_recognition.json"
+)
+
+truthfulness=(
+"external.json"
+"hallucination.json"
+"golden_advfactuality.json"
+"internal.json"
+"sycophancy.json"
+)
+
+robustness=(
+"ood_detection.json"
+"ood_generalization.json"
+"AdvGLUE.json"
+"AdvInstruction.json"
+)
+
+safety=(
+"jailbreak.json"
+"exaggerated_safety.json"
+"misuse.json"
+)
+
+ethics=(
+"awareness.json"
+"explicit_moralchoice.json"
+"implicit_ETHICS.json"
+"implicit_SocialChemistry101.json"
+)
+
+
+end_index=4
+
+num_gpus=1
+# for AREA in safety fairness truthfulness privacy robustness ethics; do
+TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
+batch_size=4
+for AREA in safety privacy; do
+    echo $AREA
+    areaArray=$AREA[@]
+    for FILE in ${!areaArray}; do
+        echo $FILE
+        DATA_NAME=${FILE%.*}
+        for MODEL in "allenai/tulu-2-7b" "allenai/tulu-2-dpo-7b" "allenai/tulu-2-dpo-13b"; do 
+        echo $MODEL
+        # CUDA_VISIBLE_DEVICES=$gpu \
+        python src/unified_infer.py \
+            --engine vllm \
+            --model_name $MODEL \
+            --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
+            --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
+            --tensor_parallel_size $num_gpus \
+            --dtype bfloat16 \
+            --top_p $TOP_P \
+            --temperature $TEMP \
+            --max_tokens $MAX_TOKENS \
+            --batch_size $batch_size \
+            --end_index $end_index \
+            --overwrite
+        done
+    done
+done
+
+
+num_gpus=4
+# for AREA in safety fairness truthfulness privacy robustness ethics; do
+TEMP=0; TOP_P=1.0; MAX_TOKENS=2048;
+batch_size=4
+for AREA in safety privacy; do
+    echo $AREA
+    areaArray=$AREA[@]
+    for FILE in ${!areaArray}; do
+        echo $FILE
+        DATA_NAME=${FILE%.*}
+        for MODEL in "allenai/tulu-2-70b" "allenai/tulu-2-dpo-70b"; do 
+        echo $MODEL
+        # CUDA_VISIBLE_DEVICES=$gpu \
+        python src/unified_infer.py \
+            --engine vllm \
+            --model_name $MODEL \
+            --output_folder ../result_dirs/trustllm/${AREA}/${DATA_NAME}/ \
+            --data_file ../../tulu-eval/TrustLLM/dataset/${AREA}/${FILE} \
+            --tensor_parallel_size $num_gpus \
+            --dtype bfloat16 \
+            --top_p $TOP_P \
+            --temperature $TEMP \
+            --max_tokens $MAX_TOKENS \
+            --batch_size $batch_size \
+            --end_index $end_index \
+            --overwrite
+        done
+    done
+done
+
