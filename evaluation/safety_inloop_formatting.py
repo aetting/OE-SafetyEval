4a126c5d68fa7adf17094c6f9458b2f41a4171f4
Allyson Ettinger <aettinger@uchicago.edu>
2024-04-03 16:07:06 -0700

    add bbq in-loop version

--- evaluation/safety_inloop_formatting.py
+++ evaluation/safety_inloop_formatting.py
@@ -1,8 +1,83 @@
 from huggingface_hub import hf_hub_download
 import pandas as pd
 import re
-from datasets import Dataset
+from datasets import Dataset, load_dataset, concatenate_datasets
 import json
+
+def access_bbq():
+    categories = [
+        "Age",
+        "Disability_status",
+        "Gender_identity",
+        "Nationality",
+        "Physical_appearance",
+        "Race_ethnicity",
+        "Religion",
+        "SES",
+        "Sexual_orientation"
+    ]
+    category_datasets = []
+    for cat in categories:
+        cat_dat = load_dataset("heegyu/bbq",split="test",name=cat).filter(lambda example: example["context_condition"] == "disambig")
+        category_datasets.append(cat_dat)
+    bbq = concatenate_datasets(category_datasets)
+    return bbq
+
+def convert_bbq_answers(answer):
+    if re.match(".*M-",answer):
+        answer = re.sub("M-","",answer)
+    if re.match(".*F-",answer):
+        answer = re.sub("F-","",answer)
+    if re.match("lowSES-",answer):
+        answer = re.sub("lowSES-","",answer)
+    if re.match("highSES-",answer):
+        answer = re.sub("highSES-","",answer)
+    elif answer in ("man","boy"):
+        answer = "M"
+    elif answer in ("woman","girl"):
+        answer = "F"
+    elif answer == "lowSES":
+        answer = "low SES"
+    elif answer == "highSES":
+        answer = "high SES"
+    elif re.match("trans",answer):
+        answer = "trans"
+    return answer
+
+def instance_conversion_bbq(instance):
+    question = f"Context: {instance['context']}\n\nQuestion: {instance['question']}\n\nAnswer: "
+    for i,a in enumerate(["ans0","ans1","ans2"]):
+        if i == instance["label"]:
+            corr = instance[a]
+        elif i != instance["label"] and instance["answer_info"][a][1] != "unknown":
+            other = instance[a]
+    continuations = [corr,other]
+    label = 0
+   
+    return question, continuations, label
+
+def get_all_bbq():
+    bbq_instances = {"pro_ster":[],"anti_ster":[]}
+    bbq = access_bbq()
+    for instance in bbq:
+        question, continuations, label = instance_conversion_bbq(instance)
+        status = None
+        label_ind = 0 if instance["category"] == "Nationality" else 1
+        ans_info = [convert_bbq_answers(instance["answer_info"][ans][label_ind]) for ans in ["ans0","ans1","ans2"]]
+        if instance["question_polarity"] == "neg":
+            if ans_info[instance["label"]] in instance["additional_metadata"]["stereotyped_groups"]:
+                status = "pro_ster"
+            else:
+                status = "anti_ster"
+        elif instance["question_polarity"] == "nonneg":
+            if ans_info[instance["label"]] in instance["additional_metadata"]["stereotyped_groups"]:
+                status = "anti_ster"
+            else:
+                status = "pro_ster"
+        if status is not None:
+            bbq_instances[status].append({'question': question,'continuations':continuations,'label':label})
+    return bbq_instances 
+
 
 def access_trustllm(datafile):
 
@@ -86,6 +161,7 @@ def instance_conversion_hallucination(instance):
 
     return question, continuations, label
 
+
 def get_all_ethics():
     ethics_instances = []
     dataset = access_trustllm("ethics/implicit_ETHICS.json")
@@ -133,8 +209,14 @@ def get_all_hallucination():
 
 if __name__ == "__main__":
     
-    ethics_instances = get_all_ethics()
-    external_truth_instances = get_all_truth_external()
-    hallucination_instances = get_all_hallucination()
-    privacy_instances = get_all_privacy()
-    stereotype_instances = get_all_stereotype()
+    # ethics_instances = get_all_ethics()
+    # external_truth_instances = get_all_truth_external()
+    # hallucination_instances = get_all_hallucination()
+    # privacy_instances = get_all_privacy()
+    # stereotype_instances = get_all_stereotype()
+    bbq_instances = get_all_bbq()
+    for i,inst in enumerate(bbq_instances["anti_ster"]):
+        if i%100 == 0:
+            print(inst)
+            print()
+    # import pdb; pdb.set_trace()
